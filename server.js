254f2877e1df8221b5636e13b094704862373758
parampreetR <raja.rai6789@gmail.com>
2020-02-15 11:19:41 +0530

    Added Comments

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const handlebars = require('express-handlebars').create({defaultLayout: 'main'})
 const port = process.env.PORT || 8080
 const app = express();
 
+//App Configuration
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 
+//Pages
 app.get('/', (req, res) => {
     res.render('home', {title: 'Home'});
 })
