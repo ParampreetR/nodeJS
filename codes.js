26050e1a4a0be25c2ad1ce44f42725fa9ae2896b
parampreetR <raja.rai6789@gmail.com>
2020-02-16 15:10:27 +0530

    Added Cookie Feature

---
+++ codes.js
@@ -0,0 +1,3 @@
+module.export = {
+    cookieSecret: '34b343fg3bergFdsafuu9834U9'
+}
--- server.js
+++ server.js
@@ -8,12 +8,33 @@ const app = express();
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/static/'))
+app.use(require('body-parser').json())
+app.use(require('body-parser').urlencoded({extended: true}))
+app.use(require('cookie-parser')('8yncr8982he8uhw87q28mfrh98dhqu'))
+
 
 //Pages
 app.get('/', (req, res) => {
     res.render('home', {title: 'Login', style: 'login.css'});
 })
 
+app.post('/', (req, res) => {
+    if(req.body.name == 'Param' && req.body.pwd == 'param'){
+        try{
+            if(req.signedCookies.Login != 'True'){
+                res.cookie('Login', 'True', {signed: true})
+                res.send('Defined Cookie Assigned')
+            }else{
+                res.send('Cookie Already Assigned')
+            }
+        }catch(TypeError){
+            res.cookie('Login', 'True', {signed: true})
+            res.send('Cookie Assigned')
+        }
+    }else{
+        res.send('Error')
+    }
+})
 
 app.listen(port, () => {
     console.log('Server Ready')
